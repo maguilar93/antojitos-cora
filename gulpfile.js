0f0a80a962cec113e84f5a6db42991009066382c
Maria Aguilar <aguilarc.maria@outlook.com>
2019-07-04 19:45:22 +0200

    fix gulpfile. fix autoprefixer. fix gulp sass. fix responsiveness in main page - spanish. fix browser-sync. fix graceful-fs. fix minimatch and lodash (errors in Github).

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 
-gulp.task("default", function() {
-    console.log("hello from inside the gulp")
+gulp.task("default", ["styles"], function() {
+  gulp.watch("sass/**/*.scss", ["styles"]);
+
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      directory: true
+    }
+  });
+});
+
+gulp.task("styles", function() {
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 });
